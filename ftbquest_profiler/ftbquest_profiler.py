aced1538590879622f760d6a4a34840139cdc3aa
FlyMachinee <1427632504@qq.com>
2026-01-20 11:33:32 +0800

    Finally implements all function

--- ftbquest_profiler/ftbquest_profiler.py
+++ ftbquest_profiler/ftbquest_profiler.py
@@ -150,7 +150,316 @@ class FTBQuestProfiler:
                 )
 
     def do_chapters(self) -> None:
-        pass
+        dir_name = "chapters"
+        dir_path = os.path.join(self.in_ftbq_dir, dir_name)
+        if not os.path.exists(dir_path):
+            self.warn(
+                f"'{dir_name}' directory not found in FTBQuests directory. Skipping."
+            )
+            return
+
+        # iter all .snbt files in the chapters directory
+        for file_name in os.listdir(dir_path):
+            if not file_name.endswith(".snbt"):
+                self.warn(
+                    f"Non-snbt file '{file_name}' found in chapters/. Skipping."
+                )
+                continue
+
+            file_path = os.path.join(dir_path, file_name)
+            try:
+                snbt_obj = self.file_to_snbt(file_path)
+            except Exception:
+                continue
+
+            self.do_chapter(file_name, snbt_obj)
+
+            # write back to output directory
+            out_file_dir_path = os.path.join(self.out_ftbq_dir, dir_name)
+            if not os.path.exists(out_file_dir_path):
+                os.makedirs(out_file_dir_path)
+            out_file_path = os.path.join(self.out_ftbq_dir, dir_name, file_name)
+            with open(out_file_path, "w", encoding="utf-8") as dst:
+                snbt_obj.pretty_file(dst)
+
+    def do_chapter(self, file_name: str, chapter: SNBT) -> None:
+        # handle chapter title
+        if "title" in chapter:
+            chapter_title = chapter["title"]
+            if not isinstance(chapter_title, String):
+                self.error(
+                    f"Chapter '{file_name}' title is not a string. Skipping."
+                )
+                return
+
+            if chapter_title:
+                new_key = f"{self.out_namespace}.chapter_{file_name[:-5]}.title"
+                self.update_out_langs(chapter_title.raw(), file_name, new_key)
+
+                # modify the snbt object
+                chapter["title"] = String(f"{{{new_key}}}")
+                # done in chapter title
+
+        # handle subtitles
+        if "subtitle" in chapter:
+            chapter_subtitles = chapter["subtitle"]
+            if not isinstance(chapter_subtitles, SNBTList):
+                self.error(
+                    f"Chapter '{file_name}' subtitle is not a list. Skipping."
+                )
+                return
+            for i, chapter_subtitle in enumerate(chapter_subtitles):
+                if not isinstance(chapter_subtitle, String):
+                    self.error(
+                        f"Chapter '{file_name}' subtitle index {i} is not a string. Skipping."
+                    )
+                    continue
+
+                if not chapter_subtitle:
+                    # skip empty subtitle
+                    continue
+
+                new_key = f"{self.out_namespace}.chapter_{file_name[:-5]}.subtitle_{i+1}"
+                self.update_out_langs(
+                    chapter_subtitle.raw(), file_name, new_key
+                )
+
+                # modify the snbt object
+                chapter_subtitles[i] = String(f"{{{new_key}}}")
+                # done in this subtitle
+
+        # handle quests
+        if "quests" in chapter:
+            quests = chapter["quests"]
+            if not isinstance(quests, SNBTList):
+                self.error(
+                    f"Chapter '{file_name}' quests is not a list. Skipping."
+                )
+                return
+            for quest in quests:
+                if not isinstance(quest, SNBT):
+                    self.warn(
+                        f"An element '{quest}' in chapter '{file_name}' quests is not a compound. Skipping."
+                    )
+                    continue
+                self.do_quest(file_name, quest)
+
+    def do_quest(self, file_name: str, quest: SNBT) -> None:
+        # get id
+        if "id" not in quest:
+            self.error(
+                f"A quest '{quest}' in chapter '{file_name}' does not have 'id'. Skipping."
+            )
+            return
+        quest_id = quest["id"]
+        if not isinstance(quest_id, String):
+            self.error(
+                f"Quest id '{quest_id}' in chapter '{file_name}' is not a string. Skipping."
+            )
+            return
+
+        # handle quest title and subtitle
+        for key in ["title", "subtitle"]:
+            if key in quest:
+                value = quest[key]
+                if not isinstance(value, String):
+                    self.error(
+                        f"Quest '{quest_id.raw()}' {key} in chapter '{file_name}' is not a string. Skipping."
+                    )
+                    continue
+
+                if not value:
+                    # skip empty string
+                    continue
+
+                new_key = f"{self.out_namespace}.chapter_{file_name[:-5]}.quest_{quest_id.raw()}.{key}"
+                self.update_out_langs(value.raw(), file_name, new_key)
+
+                # modify the snbt object
+                quest[key] = String(f"{{{new_key}}}")
+                # done in this key
+
+        # handle tasks and rewards title
+        for key in ["tasks", "rewards"]:
+            if key in quest:
+                items = quest[key]
+                if not isinstance(items, SNBTList):
+                    self.error(
+                        f"Quest '{quest_id.raw()}' {key} in chapter '{file_name}' is not a list. Skipping."
+                    )
+                    continue
+                for item in items:
+                    if not isinstance(item, SNBT):
+                        self.warn(
+                            f"An element '{item}' in quest '{quest_id.raw()}' {key} in chapter '{file_name}' is not a compound. Skipping."
+                        )
+                        continue
+
+                    # get id
+                    if "id" not in item:
+                        self.error(
+                            f"An item '{item}' in quest '{quest_id.raw()}' {key} in chapter '{file_name}' does not have 'id'. Skipping."
+                        )
+                        continue
+                    item_id = item["id"]
+                    if not isinstance(item_id, String):
+                        self.error(
+                            f"An item id '{item_id}' in quest '{quest_id.raw()}' {key} in chapter '{file_name}' is not a string. Skipping."
+                        )
+                        continue
+
+                    if "title" in item:
+                        item_title = item["title"]
+                        if not isinstance(item_title, String):
+                            self.error(
+                                f"Item '{item_id.raw()}' title in quest '{quest_id.raw()}' {key} in chapter '{file_name}' is not a string. Skipping."
+                            )
+                            continue
+
+                        if not item_title:
+                            # skip empty title
+                            continue
+
+                        new_key = f"{self.out_namespace}.chapter_{file_name[:-5]}.quest_{quest_id.raw()}.{key[:-1]}_{item_id.raw()}.title"
+                        self.update_out_langs(
+                            item_title.raw(), file_name, new_key
+                        )
+
+                        # modify the snbt object
+                        item["title"] = String(f"{{{new_key}}}")
+                        # done in this item title
+
+        # handle quest description
+        if "description" not in quest:
+            return
+
+        descriptions = quest["description"]
+        if not isinstance(descriptions, SNBTList):
+            self.error(
+                f"Quest '{quest_id.raw()}' description in chapter '{file_name}' is not a list. Skipping."
+            )
+            return
+
+        counter = 1
+        for i, desc in enumerate(descriptions):
+            if not isinstance(desc, String):
+                self.error(
+                    f"A description entry '{desc}' in quest '{quest_id.raw()}' in chapter '{file_name}' is not a string. Skipping."
+                )
+                continue
+
+            if not desc or desc == "{@pagebreak}":
+                # skip empty description or pagebreak
+                continue
+
+            raw_desc = desc.raw()
+
+            try:
+                decoded_desc = (
+                    raw_desc.replace("\\n", "\n")
+                    .replace("\\r", "\r")
+                    .replace("\\t", "\t")
+                    .replace('\\"', '"')
+                    .replace("\\\\", "\\")
+                )
+                json_obj = json.loads(decoded_desc)
+                is_json = True
+            except Exception:
+                # not a json
+                json_obj = None
+                is_json = False
+
+            if not is_json:
+                new_key = f"{self.out_namespace}.chapter_{file_name[:-5]}.quest_{quest_id.raw()}.desc_{counter:02d}"
+                self.update_out_langs(raw_desc, file_name, new_key)
+
+                # modify the snbt object
+                descriptions[i] = String(f"{{{new_key}}}")
+                # done in this description
+                counter += 1
+                continue
+
+            # is a json object
+            if not json_obj:
+                # empty json object
+                continue
+
+            def do_json_text(json_obj: dict, rich_num: int) -> bool:
+                if "translate" in json_obj:
+                    rich_text = json_obj["translate"]
+                    if not isinstance(rich_text, str):
+                        self.error(
+                            f"The 'translate' field in description entry '{desc.raw()}' in quest '{quest_id.raw()}' in chapter '{file_name}' is not a string. Skipping."
+                        )
+                        return False
+                    # assume it's a key
+                    rich_text = f"{{{rich_text}}}"
+                elif "text" not in json_obj:
+                    self.warn(
+                        f"A description entry '{desc.raw()}' in quest '{quest_id.raw()}' in chapter '{file_name}' JSON object does not contain 'text'. Skipping."
+                    )
+                    return False
+                else:
+                    rich_text = json_obj["text"]
+                    if not isinstance(rich_text, str):
+                        self.error(
+                            f"The 'text' / 'translate' field in description entry '{desc.raw()}' in quest '{quest_id.raw()}' in chapter '{file_name}' is not a string. Skipping."
+                        )
+                        return False
+
+                if not rich_text:
+                    # skip empty text
+                    return False
+
+                new_key = f"{self.out_namespace}.chapter_{file_name[:-5]}.quest_{quest_id.raw()}.desc_{counter:02d}.rich_{rich_num:02d}"
+                self.update_out_langs(rich_text, file_name, new_key)
+                if "text" in json_obj:
+                    del json_obj["text"]
+                json_obj["translate"] = new_key
+                return True
+
+            success = False
+            if isinstance(json_obj, dict):
+                if do_json_text(json_obj, 1):
+                    success = True
+            elif isinstance(json_obj, list):
+                rich_num = 1
+                for j, elem in enumerate(json_obj):
+                    if isinstance(elem, dict):
+                        if do_json_text(elem, rich_num):
+                            rich_num += 1
+                            success = True
+                    elif isinstance(elem, str):
+                        # raw text in array
+                        if not elem:
+                            continue
+
+                        new_key = f"{self.out_namespace}.chapter_{file_name[:-5]}.quest_{quest_id.raw()}.desc_{counter:02d}.rich_{rich_num:02d}"
+                        self.update_out_langs(elem, file_name, new_key)
+                        json_obj[j] = {"translate": new_key}
+                        rich_num += 1
+                        success = True
+                if not isinstance(json_obj[0], str):
+                    # insert an empty string at the beginning to avoid issues
+                    json_obj.insert(0, "")
+            else:
+                self.error("Control flow should not reach here.")
+                continue
+
+            if success:
+                # modify the snbt object
+                new_desc_str = json.dumps(json_obj, ensure_ascii=False)
+                escaped_desc_str = (
+                    new_desc_str.replace("\\", "\\\\")
+                    .replace("\n", "\\n")
+                    .replace("\r", "\\r")
+                    .replace("\t", "\\t")
+                    .replace('"', '\\"')
+                )
+                descriptions[i] = String(escaped_desc_str)
+                # done in this description
+                counter += 1
+                continue
 
     def do_reward_tables(self) -> None:
         dir_name = "reward_tables"
